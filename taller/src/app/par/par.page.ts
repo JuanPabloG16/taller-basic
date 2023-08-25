69736f4ed5f110a56fd30329c2c17c5c3d5d14a8
JuanPabloG16 <93049031+JuanPabloG16@users.noreply.github.com>
2023-08-25 17:35:42 -0500

    parte 3
    pares e impares hechos

--- taller/src/app/par/par.page.ts
+++ taller/src/app/par/par.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 @Component({
   selector: 'app-par',
   templateUrl: './par.page.html',
@@ -11,10 +10,16 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ParPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  userNumber: number = 0;
+  selectedOption: 'pares' | 'impares' = 'pares';
+  resultArray: number[] = [];
+  constructor() {}
+  ngOnInit() {}
+  calculate() {
+    this.resultArray = [];
+    const start = this.selectedOption === 'pares' ? 2 : 1; 
+    for (let i = start; i <= this.userNumber; i += 2) {
+      this.resultArray.push(i);
+    }
   }
-
 }
